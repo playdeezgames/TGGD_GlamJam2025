2ea41b241a78eaae8387975a7b70d62db4dc22f2
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-01-02 05:56:47 -0600

    done for the day

--- Squeeeee/board/Board.go
+++ Squeeeee/board/Board.go
@@ -9,3 +9,27 @@ components {
   id: "Board1"
   component: "/board/Board.script"
 }
+components {
+  id: "score_label"
+  component: "/board/score_label.label"
+  position {
+    x: 80.0
+    y: 16.0
+  }
+}
+components {
+  id: "group_label"
+  component: "/board/group_label.label"
+  position {
+    x: 80.0
+    y: 48.0
+  }
+}
+components {
+  id: "level_label"
+  component: "/board/level_label.label"
+  position {
+    x: 80.0
+    y: 80.0
+  }
+}
